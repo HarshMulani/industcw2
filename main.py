d92aee326d4b9672c0df6f156c76f145015f8464
Mulani, Harsh M <hmm7@hw.ac.uk>
2022-12-07 02:38:23 +0000

    Task 7

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import os
+import tkinter.filedialog as fd
+import tkinter.scrolledtext as st
+import tkinter.ttk as ttk
+import tkinter as tk
 import pandas as pd
 # import tkinter as tk
 # import tkinter.ttk as ttk
@@ -24,6 +29,7 @@ def load_data(entryList):
         df.loc[i] = [entryList[i]["ts"], entryList[i]
                      ["env_doc_id"], entryList[i]['visitor_country'], entryList[i]['visitor_useragent'], entryList[i]['visitor_uuid'], entryList[i]['event_readtime']]
     return df
+
 
 def read_from_file(file):
     entryList = []
@@ -92,6 +98,8 @@ def plot_data_browser(df):
     plt.show()
 
 # Determines the readership profiles for users, constructing an object of the user-id and their reading time, prints out the top ten readers
+
+
 def determine_readership(df):
     resultdict = {}
     templist = []
@@ -170,12 +178,13 @@ def get_readers_doc_list(reader_list, df):
     print(return_dict)
     return return_dict
 
+
 def get_also_likes(df, doc):
     # call the reader_list and user_list funcitons to compile the also_liked list.
     users = get_doc_reader_list(doc, df)
     also_liked = get_readers_doc_list(users, df)
 
-     # Separetes the keys and values into their own respective lists
+    # Separetes the keys and values into their own respective lists
     keys = list(also_liked.keys())
     values = list(also_liked.values())
     # Create a sorted index of the values from the previously gatehred values
@@ -195,8 +204,9 @@ def get_also_likes(df, doc):
     top_ten_reverse = OrderedDict(reversed(list(top_ten.items())))
     return top_ten_reverse
 
+
 def plot_also_likes(also_liked, doc):
-    
+
     # Separate the keys and values in order to get them in a list format to use in the plot
     keys = also_liked.keys()
     values = also_liked.values()
@@ -208,6 +218,84 @@ def plot_also_likes(also_liked, doc):
     plt.title("Documents also liked for " + doc)
     # Finally show the plot
     plt.show()
+
+
+def gui():
+    root_window = tk.Tk()
+    root_window.title("Team 11's Issuu Data Analysis Application")
+    root_window.geometry("900x400")
+    root_window.maxsize(900, 400)
+    root_window.minsize(900, 400)
+    root_window.configure(bg="#FCF1EB")
+    label1 = ttk.Label(root_window, text="Task Results", font=(
+        "Calibri", 12), background="#FCF1EB")
+    label1.grid(row=0, column=2, padx=10, pady=5)
+
+    result_text_area = st.ScrolledText(
+        root_window, width=30, height=20)
+    result_text_area.grid(column=2, pady=5, padx=5, rowspan=10)
+    result_text_area.configure(state="disabled")
+
+    label_dataset = ttk.Label(root_window, text="Dataset: ", font=(
+        "Calibri", 12), background="#FCF1EB")
+    label_dataset.grid(row=1, column=3, padx=10, pady=5)
+
+    global dataset_input
+    dataset_input = ttk.Entry(root_window, width=55)
+    dataset_input.grid(row=1, column=4, padx=5, pady=5, columnspan=2)
+    dataset_input.configure(state="readonly")
+
+    file_button = ttk.Button(
+        root_window, text="Select JSON", command=open_file)
+    file_button.grid(row=1, column=6, padx=5, pady=5)
+
+    label_document = ttk.Label(root_window, text="Document UUID: ", font=(
+        "Calibri", 12), background="#FCF1EB")
+    label_document.grid(row=2, column=3, padx=5, pady=5)
+    document_input = ttk.Entry(root_window, width=55)
+    document_input.grid(row=2, column=4, padx=5, pady=5, columnspan=1)
+    document_input.configure(state="normal")
+
+    label_user = ttk.Label(root_window, text="User UUID: ", font=(
+        "Calibri", 12), background="#FCF1EB")
+    label_user.grid(row=3, column=3, padx=5, pady=5)
+    user_input = ttk.Entry(root_window, width=55)
+    user_input.grid(row=3, column=4, padx=5, pady=5, columnspan=1)
+    user_input.configure(state="normal")
+
+    btn_2a = ttk.Button(root_window, text="2a. Views by Country")
+    btn_2a.grid(row=4, column=3, padx=5, pady=5)
+
+    btn_2b = ttk.Button(root_window, text="2b. Views by Continent")
+    btn_2b.grid(row=5, column=3, padx=5, pady=5)
+
+    btn_3a = ttk.Button(root_window, text="3a. Useragent Data")
+    btn_3a.grid(row=4, column=4, padx=5, pady=5)
+
+    btn_3b = ttk.Button(root_window, text="3b. Views by Browser")
+    btn_3b.grid(row=5, column=4, padx=5, pady=5)
+
+    btn_4 = ttk.Button(root_window, text="4. View Frequent Readers")
+    btn_4.grid(row=6, column=4, padx=5, pady=5)
+
+    btn_5 = ttk.Button(root_window, text="5. Also-Likes")
+    btn_5.grid(row=4, column=6, padx=5, pady=5)
+
+    btn_5 = ttk.Button(root_window, text="6. Also-Likes Graph")
+    btn_5.grid(row=5, column=6, padx=5, pady=5)
+
+    root_window.mainloop()
+
+
+def open_file():
+    file_name = fd.askopenfile(mode='r',
+                               initialdir="/", title="Please select JSON File.", filetypes=(("JSON", "*.json"), ("JSON", "*.json")))
+    if file_name:
+        filepath = os.path.abspath(file_name.name)
+        dataset_input.configure(state="normal")
+        dataset_input.insert(0, filepath)
+        return filepath
+
 
 def main():
     # Initialise the parser using the argparser library to simplify the calling
@@ -235,18 +323,18 @@ def main():
             plot_data_country(file_data, args.udid)
         else:
             print("To execute the histogram of document views by country, please add this after the -d or -udid flag")
-    elif args.task ==  '2b':
+    elif args.task == '2b':
         if args.udid:
             plot_data_continent(file_data, args.udid)
         else:
             print("To execute the histogram of document views by continent, please add this after the -d or -udid flag")
-    elif args.task ==  '3a':
+    elif args.task == '3a':
         plot_data_browser(file_data)
-    elif args.task ==  '3b':
+    elif args.task == '3b':
         plot_data_useragent(file_data)
-    elif args.task ==  '4':
+    elif args.task == '4':
         get_readership(file_data=file_data)
-    elif args.task ==  '5d':
+    elif args.task == '5d':
         if args.udid:
             top_ten = get_also_likes(file_data, args.udid)
             print("Top ten related docs are: ")
@@ -254,14 +342,14 @@ def main():
                 print(entry)
         else:
             print("A unique document id is required to run this function, please provide that after the -u tag or --udid tag")
-    elif args.task ==  '6':
+    elif args.task == '6':
         if args.udid:
             top_ten = get_also_likes(file_data, args.udid)
             plot_also_likes(top_ten, args.udid)
         else:
             print("A unique document id is required to run this function, please provide that after the -u tag or --udid tag")
-    elif args.task ==  '7':
-        print(True)
+    elif args.task == '7':
+        gui()
     # If no task is given let the user know what was wrong and the tasks they can implement.
     else:
         print("Please input a flag to denote task, either -t or --task followed by:")
